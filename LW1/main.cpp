5a2413ef7cc93c774a8aebb4008e71ddb3f736eb
AVAtarMod <Respawner289@gmail.com>
2023-02-10 12:57:54 +0300

    Add template menu

---
+++ LW1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void task1();
+
+int main(int argc, char const *argv[])
+{
+   bool isArgsEmpty = false;
+   if (argc <= 1)
+   {
+      isArgsEmpty = true;
+      std::cout << "Enter program number to start: ";
+      argc++;
+   }
+   for (int i = 1; i < argc; i++)
+   {
+      int choice;
+      if (isArgsEmpty)
+      {
+         std::cin >> choice;
+         std::cin.ignore(32767, '\n');
+      }
+      else
+      {
+         choice = std::stoi(argv[i]);
+      }
+
+      switch (choice)
+      {
+      case 1:
+         std::cout << "Task #1\n";
+         task1();
+         std::cout << "-----\n";
+         break;
+      default:
+         std::cerr << "Error: task number is unsupported or cannot be recognized.\n";
+         break;
+      }
+   }
+}
+
+void task1() {}
