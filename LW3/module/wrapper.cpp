413f9e9bc41834d3a72dee0c37f2a5ea0ec72c85
AVAtarMod <Respawner289@gmail.com>
2023-03-02 20:35:37 +0300

    Update wrapper

--- LW3/module/wrapper.cpp
+++ LW3/module/wrapper.cpp
@@ -1,16 +1,13 @@
 #include <pybind11/pybind11.h>
-#include "Point.hpp"
-#include ""
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(lib_cppgeometry_wrapper, m)
-{
-   m.doc() = "pybind11 example plugin"; // optional module docstring
+int add(int i, int j) {
+    return i + j;
+}
 
-   m.def("add",
-         &add,
-         "A function that adds two numbers",
-         py::arg("i"),
-         py::arg("j"));
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
 }
