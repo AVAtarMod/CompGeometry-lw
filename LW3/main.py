af46cdcdbb7f5f3dde8483fe487560dabe98994a
AVAtarMod <Respawner289@gmail.com>
2023-03-06 14:21:11 +0300

    LW3: update gui, wrapper

--- LW3/main.py
+++ LW3/main.py
@@ -3,31 +3,56 @@ import dearpygui_ext.themes as dpg_ext
 import dearpygui.dearpygui as dpg
 import gui
 
+def update_text(item_id):
+   def __update(sender):
+      val = dpg.get_value(sender)
+      dpg.set_value(item_id, "x: {:.2f},y: {:.2f}".format(
+          round(val[0],2), round(val[1],2)))
+   return __update
 
 def window():
+   labels = "ABCDEFGHIJKLMPRSTUFXYZ"
+   used = []
+
    def print_me(sender):
       print(f"Menu Item: {sender}")
-   w = dpg.add_window(label="Tutorial")
+   w = dpg.add_window(label="main window")
 
    with dpg.menu_bar(parent=w):
-      with dpg.menu(label="Сгенерировать точки"):
-         dpg.add_menu_item(label="Save", callback=print_me)
-         dpg.add_menu_item(label="Save As", callback=print_me)
-
-         # with dpg.menu(label="Settings"):
-         #    dpg.add_menu_item(label="Setting 1",
-         #                      callback=print_me, check=True)
-         #    dpg.add_menu_item(label="Setting 2", callback=print_me)
+      dpg.add_menu_item(label="Сгенерировать точки")
 
       dpg.add_menu_item(label="Построить выпуклую оболочку", callback=print_me)
 
+      with dpg.menu(label="Настройки"):
+         dpg.add_menu_item(label="Setting 1",
+                           callback=print_me, check=True)
+         dpg.add_menu_item(label="Setting 2", callback=print_me)
+
       dpg.add_menu_item(label="Помощь")
+   p = l.Point.getRandom(0, 3, 2)
+   x,y = p["x"],p["y"]
+   text = dpg.add_text(f"x: {x},y: {y}",parent=w)
+   with dpg.plot(label="График", height=400, width=-1, parent=w):
+      dpg.add_plot_legend()
+      oX = dpg.add_plot_axis(dpg.mvXAxis, label="x")
+      oY = dpg.add_plot_axis(dpg.mvYAxis, label="y")
+
+      # drag lines/points belong to the plot NOT axis
+      # dpg.add_drag_line(
+      #    label="dline1", color=[255, 0, 0, 255])
+      # dpg.add_drag_line(label="dline2", color=[
+      #    255, 255, 0, 255], vertical=False)
+
+      dpg.add_drag_point(
+         label="dpoint1", color=[255, 0, 255, 255],default_value=(p["x"],p["y"]),callback=update_text(text))
+      # dpg.add_drag_point(
+      #    label="dpoint2", color=[255, 0, 255, 255])
    dpg.set_primary_window(w, True)
 
 
 def viewport():
    dpg.create_viewport(title='ConvexHull GUI',
-                       width=800, height=600)
+                       width=1000, height=600)
 
 
 def fix_font(id: str | int):
--- LW3/module/wrapper.cpp
+++ LW3/module/wrapper.cpp
@@ -8,10 +8,14 @@ PYBIND11_MODULE(lib_cppgeometry_wrapper, m)
    m.doc() = "lib_cppgeometry wrapper for Python"; // optional module docstring
 
    py::class_<Point>(m, "Point")
-     .def(py::init<int>())
-     .def(py::init<const Point&>())
-     .def(py::init<double, double>())
+     .def(py::init<int>(), "Construct N-dimension Point")
+     .def(py::init<const Point&>(), "Copy constructor")
+     .def(py::init<double, double>(), "Construct 2-dimension Point")
      .def("size", &Point::size)
      .def("dimension", &Point::size)
-     .def("getRandom", &Point::getRandom);
+     .def("getRandom", &Point::getRandom)
+     .def("__getitem__",
+          py::overload_cast<const char*>(&Point::operator[], py::const_))
+     .def("__setitem__",
+          py::overload_cast<const char*>(&Point::operator[]));
 }
