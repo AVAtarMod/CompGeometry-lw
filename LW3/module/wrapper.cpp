fc0a529a5405bd3c3b6a9b88f6f067d598cc8a78
AVAtarMod <Respawner289@gmail.com>
2023-03-20 14:07:27 +0300

    LW3: update wrapper

--- LW3/module/wrapper.cpp
+++ LW3/module/wrapper.cpp
@@ -1,7 +1,11 @@
 #include "lib_cppgeometry/Point.hpp"
-#include <pybind11/pybind11.h>
+#include "lib_cppgeometry/Polygon.hpp"
 
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
 namespace py = pybind11;
+
+PYBIND11_MAKE_OPAQUE(std::vector<Point>);
 
 PYBIND11_MODULE(lib_cppgeometry_wrapper, m)
 {
@@ -16,6 +20,19 @@ PYBIND11_MODULE(lib_cppgeometry_wrapper, m)
      .def("getRandom", &Point::getRandom)
      .def("__getitem__",
           py::overload_cast<const char*>(&Point::operator[], py::const_))
-     .def("__setitem__",
-          py::overload_cast<const char*>(&Point::operator[]));
+     .def("__setitem__", py::overload_cast<const char*>(&Point::operator[]));
+
+   py::class_<Polygon>(m, "Polygon")
+     .def("convexHull", &Polygon::convexHull)
+     .def("size", &Polygon::size)
+     .def("__getitem__",
+          py::overload_cast<int>(&Polygon::operator[], py::const_))
+     .def("__setitem__", py::overload_cast<int>(&Polygon::operator[]));
+
+   py::enum_<ConvexHullMethod>(m, "ConvexHullMethod")
+     .value("GRAHAM", ConvexHullMethod::GRAHAM)
+     .value("JARVIS", ConvexHullMethod::JARVIS)
+     .export_values();
+
+   py::bind_vector<std::vector<Point>>(m, "VectorPoint");
 }
