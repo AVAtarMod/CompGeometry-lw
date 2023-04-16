313e03a694a90cfd807d096a84ef02a8b3099ade
AVAtarMod <Respawner289@gmail.com>
2023-04-16 19:52:41 +0300

    LW3: update module test

--- LW3/module/main.cpp
+++ LW3/module/main.cpp
@@ -10,11 +10,11 @@
 
 int main(int argc, char const* argv[])
 {
-   std::vector<Point> test = { Point(2, 3),
-                               Point(0, 0),
-                               Point(2,0),
-                               Point(1.5, 1.6),
-                               Point(0, 2) };
+   std::vector<Point> test = { Point(9.60817, 11.1311),
+                               Point(15.66, 10.08),
+                               Point(21.22, 18.11),
+                               Point(18.77, 15.82),
+                               Point(1.88, 21.39) };
    auto i = Polygon::convexHull(test, ConvexHullMethod::JARVIS);
    Point a = i[0];
    return 0;
