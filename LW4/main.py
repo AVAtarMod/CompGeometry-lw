6e0c1df8cdd97365e536313efa5d2e2b651fc150
AVAtarMod <Respawner289@gmail.com>
2023-05-09 11:23:56 +0300

    LW4: Add GUI. Fix font usage

--- LW4/main.py
+++ LW4/main.py
@@ -1,14 +1,23 @@
 import lib_cppgeometry_wrapper as l
 import dearpygui_ext.themes as dpg_ext
 import dearpygui.dearpygui as dpg
-from typing import Callable
 import sys
 from vars import *
 from pathlib import Path
 import importlib
 from event_handlers import *
 
-e_pool = {}
+bind_pool = {}
+"""Array of EventPoolHandler's for binding only
+"""
+dyn_pool = {}
+"""Array of EventPoolHandler's for dynamically generated
+   UI elements
+"""
+s_pool = {}
+"""Array of EventPoolHandler's for static UI elements
+"""
+
 
 def import_parents(level=1):
    global __package__
@@ -32,27 +41,32 @@ if __name__ == '__main__' and __package__ is None or __package__ == '':
 
 
 def window():
-   global ui_text, plot_number_amount
+   global ui_text, plot_number_amount, listbox_id
 
-   global button_bar_gen, button_make_hull, \
-       input_point_amount, input_method, text, plot, __points, \
+   global button_bar_gen, button_clip, \
+       input_point_amount, text, plot, \
        input_min_val, input_max_val, input_min_valm, input_theme
-   __points = {}
 
    w = dpg.add_window()
 
    with dpg.menu_bar(parent=w):
-      button_bar_gen = dpg.add_menu_item(label="Сгенерировать точки")
+      button_bar_gen = dpg.add_menu_item(label="Сгенерировать данные")
 
-      button_make_hull = dpg.add_menu_item(
-          label="Построить выпуклую оболочку", enabled=False)
+      button_clip = dpg.add_menu_item(
+          label="Обрезать", enabled=False)
 
       with dpg.menu(label="Настройки"):
          with dpg.group(horizontal=True):
             dpg.add_text(default_value="Количество точек: ")
+            input_point_amount = dpg.add_input_int(
+                width=120, default_value=plot_number_amount)
+            dpg.add_text(
+                default_value=f"(по умолчанию: {plot_number_amount})")
+
          with dpg.group(horizontal=True):
-            dpg.add_text(default_value="Метод построения: ")
-            input_method = dpg.add_listbox(items=list(method_map.keys()))
+            dpg.add_text(default_value="Метод обрезания: ")
+            listbox_id = dpg.add_listbox(
+                items=list(method_map.keys()))
          dpg.add_text(default_value="Параметры генерации точек: ")
          with dpg.group(horizontal=True):
             dpg.add_text(default_value="Min: ")
@@ -71,32 +85,40 @@ def window():
 
 
 def set_callbacks():
-   global __points
    # Static UI objects pools
-   e_pool["virt_w_text"] = EventHandlerPool(text, False)
-   e_pool["stc_btn_point_gen"] = EventHandlerPool(button_bar_gen)
-   e_pool["stc_settings_method"] = EventHandlerPool(input_method)
-   e_pool["stc_settings_min_val"] = EventHandlerPool(input_min_val)
-   e_pool["stc_settings_max_val"] = EventHandlerPool(input_max_val)
-   e_pool["stc_settings_theme"] = EventHandlerPool(input_theme)
-   e_pool["stc_btn_make_hull"] = EventHandlerPool(button_make_hull)
+   bind_pool["w_text"] = EventHandlerPool(text, False)
+   s_pool["btn_point_gen"] = EventHandlerPool(button_bar_gen)
+   s_pool["settings_point_amount"] = EventHandlerPool(input_point_amount)
+   s_pool["settings_method"] = EventHandlerPool(listbox_id)
+   s_pool["settings_min_val"] = EventHandlerPool(input_min_val)
+   s_pool["settings_max_val"] = EventHandlerPool(input_max_val)
+   s_pool["settings_theme"] = EventHandlerPool(input_theme)
+   s_pool["btn_clip"] = EventHandlerPool(button_clip)
    # Dynamic point pool
-   e_pool["dyn_point"] = EventHandlerPool(0, False)
+   dyn_pool["frame_point"] = EventHandlerPool(0, False)
 
    # Pool configuration
-   e_pool["dyn_point"] += e_pool["virt_w_text"].get_bind_handler(
-       0, PointHandlers.process_to_text, use_sender=True)
-   e_pool["dyn_point"] += PointHandlers.update_lines()
-   e_pool["dyn_point"] += PointHandlers.update_points()
-   e_pool["dyn_point"] += PointHandlers.print_points()
+   if debug:
+      dyn_pool["frame_point"] += bind_pool["w_text"].get_bind_handler(
+          0, PointHandlers.process_to_text, use_sender=True)
+      # dyn_pool["frame_point"] += PointHandlers.print_points()
 
-   e_pool["stc_btn_point_gen"] += ButtonHandlers.generate_point(
-       plot, e_pool["dyn_point"], [button_make_hull])
-   e_pool["stc_settings_method"] += ListBoxHandlers.set_current_method
-   e_pool["stc_btn_make_hull"] += ButtonHandlers.make_hull_by_points
-   e_pool["stc_settings_min_val"] += TextHandlers.point_set_min_val
-   e_pool["stc_settings_max_val"] += TextHandlers.point_set_max_val
-   e_pool["stc_settings_theme"] += ListBoxHandlers.set_theme
+   s_pool["settings_point_amount"] += TextHandlers.set_plot_number_amount(
+       listbox_id)
+   dyn_pool["frame_point"] += PointHandlers.update_frame(
+       plot, dyn_pool["frame_point"])
+
+   s_pool["btn_point_gen"] += ButtonHandlers.generate_frame(
+       plot, dyn_pool["frame_point"])
+   s_pool["btn_point_gen"] += ButtonHandlers.generate_clip_line(plot)
+   s_pool["btn_point_gen"] += ButtonHandlers.enable_items([button_clip])
+   s_pool["settings_method"] += ListBoxHandlers.set_current_method
+   s_pool["btn_clip"] += ButtonHandlers.clip_line(
+       TextHandlers.notify_segment_empty, plot)
+   s_pool["settings_min_val"] += TextHandlers.point_set_min_val
+   s_pool["settings_max_val"] += TextHandlers.point_set_max_val
+   s_pool["settings_theme"] += ListBoxHandlers.set_theme
+
 
 def viewport():
    dpg.create_viewport(title='ConvexHull GUI',
@@ -109,11 +131,11 @@ def fix_font(id: str | int):
 
 def main():
    theme = list(themes.values())[0]
-   font = [sys.path[0] + "/assets/Montserrat-Regular.otf", 25]
+   font = ["../assets/Montserrat-Regular.otf", 25]
 
    items = [window, viewport, set_callbacks]
    gui.create_gui(gui.Parameters(
-       items, theme, font[0], fix_font, font[1], True))
+       items, theme, font[0], fix_font, font[1], debug))
 
 
 if __name__ == "__main__":
