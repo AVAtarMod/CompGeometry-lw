aa465c41ed5c038b01828d44e0d22d3393ed649e
Yura-Zh43 <yura10295@mail.ru>
2023-05-18 19:36:26 +0300

    Исправлен CMake и начата 6-я лаба

--- LW5_6/part2.cpp
+++ LW5_6/part2.cpp
@@ -1,20 +1,64 @@
 #include "part2.hpp"
-#include <fstream>
-#include <iostream>
-#include <vector>
 
 namespace t2 {
+   std::vector<Point> read(std::ifstream& in)
+   {
+      int testsCount;
+      double x, y;
+      in >> testsCount;
+      std::vector<Point> p(testsCount);
+      for (int i = 0; i < testsCount; i++) {
+         in >> x >> y;
+         p[i] = Point(x, y);
+      }
+      return p;
+   }
+
+   matrix_t readMat(std::ifstream& in)
+   {
+      int n, i, j;
+      double x, y;
+      in >> n;
+      matrix_t mat = matrix_t(n);
+      for (i = 0; i < n; i++) {
+         mat[i] = std::vector<int>(n);
+         for (j = 0; j < n; j++)
+            in >> mat[i][j];
+      }
+      return mat;
+   }
 } // namespace t2
 
 void task2()
 {
-   const char* in_file = "task2in.txt";
-   std::ifstream in(in_file);
+   const char* in_file1 = "1.txt";
+   std::ifstream in(in_file1);
+   const char* in_file2 = "2.txt";
+   std::ifstream in_mat(in_file2);
 
    if (!in) {
-      std::cerr << "File " << in_file << " not exist or not accessible\n";
+      std::cerr << "File " << in_file1 << " not exist or not accessible\n";
    } else {
-      // code here
+      if (!in_mat) {
+         std::cerr << "File " << in_file2 << " not exist or not accessible\n";
+      } else {
+         std::vector<Point> points = t2::read(in);
+         matrix_t mat = t2::readMat(in_mat);
+
+         double x, y;
+         cout << "Enter point coordinates: ";
+         cin >> x >> y;
+
+         Point p(x, y);
+         Graph g(mat, points);
+         std::unique_ptr<Polygon> pol = g.localizationOfAPoint(p);
+         if (pol)
+            for (int i = 0; i < pol->size(); i++)
+               std::cout << (*pol)[i] << std::endl;
+         else
+            std::cout << "The point is outside of the graph!" << std::endl;
+      }
+      in_mat.close();
    }
    in.close();
 }
--- LW5_6/part2.hpp
+++ LW5_6/part2.hpp
@@ -1,6 +1,14 @@
 #ifndef LW1_PART2_HPP
 #define LW1_PART2_HPP
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "lib_cppgeometry/Point.hpp"
+#include "lib_cppgeometry/Graph.hpp"
+
+using namespace std;
+
 void task2();
 
 #endif // LW1_PART2_HPP
