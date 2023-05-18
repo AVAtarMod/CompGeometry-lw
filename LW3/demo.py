db08d6d3025b317736d7de46df577e5238497a16
AVAtarMod <Respawner289@gmail.com>
2023-05-18 21:09:14 +0300

    LW3: Fix demo

--- LW3/demo.py
+++ LW3/demo.py
@@ -1,6 +1,30 @@
-import gui
+import sys
 import dearpygui.demo as d
 import dearpygui.dearpygui as dpg
+from pathlib import Path
+import importlib
+
+def import_parents(level=1):
+   global __package__
+   file = Path(__file__).resolve()
+   parent, top = file.parent, file.parents[level]
+
+   sys.path.append(str(top))
+   try:
+      sys.path.remove(str(parent))
+   except ValueError:  # already removed
+      pass
+
+   __package__ = '.'.join(parent.parts[len(top.parts):])
+   importlib.import_module(__package__)  # won't be needed after that
+
+
+if __name__ == '__main__' and __package__ is None or __package__ == '':
+   import_parents(level=2)
+   from ..lib.gui import themes as t
+   from ..lib.gui import gui
+   from ..lib.gui.events import EventHandlerPool
+
 
 def demo():
    d.show_demo()
