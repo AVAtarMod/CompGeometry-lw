350c3251aa1f14cbe30ce5eea01733c6a8e932b1
AVAtarMod <Respawner289@gmail.com>
2023-05-24 16:51:03 +0300

    LW5_6: Fix parsing; Replace line endings to LF

--- LW5_6/part1.cpp
+++ LW5_6/part1.cpp
@@ -6,39 +6,85 @@
 #include "lib_cppgeometry/Polygon.hpp"
 #include "part1.hpp"
 
-Polygon makePolygonByArea(const std::pair<double, double>& x_minmax, const std::pair<double, double>& y_minmax) {
-    const Point a(x_minmax.first, y_minmax.first), b(x_minmax.second, y_minmax.first), c(x_minmax.second, y_minmax.second), d(x_minmax.first, y_minmax.second);
-    return Polygon(std::vector<Point>{a,b,c,d});
+Polygon makePolygonByArea(const std::pair<double, double>& x_minmax,
+                          const std::pair<double, double>& y_minmax)
+{
+   const Point a(x_minmax.first, y_minmax.first),
+     b(x_minmax.second, y_minmax.first), c(x_minmax.second, y_minmax.second),
+     d(x_minmax.first, y_minmax.second);
+   return Polygon(std::vector<Point> { a, b, c, d });
 }
 
-struct InputData {
-    Polygon polygon = Polygon(std::vector<Point>());
-    std::vector<Point> points;
+struct InputData
+{
+   Polygon polygon = Polygon(std::vector<Point>());
+   std::vector<Point> points;
 };
+
+struct IOChar
+{
+   char data = '\0';
+   bool success = false;
+};
+
+IOChar try_get(std::ifstream& in)
+{
+   IOChar result;
+   in.get(result.data);
+   if (in.fail() || in.gcount() == 0)
+      result.success = false;
+   else
+      result.success = true;
+   return result;
+}
+
+double next_number(std::ifstream& in)
+{
+   const size_t BUFFER_SIZE = 255;
+   std::string tmp;
+   tmp.resize(BUFFER_SIZE);
+
+   double result;
+   while (true) {
+      IOChar input = try_get(in);
+      if (!input.success)
+         break;
+      if (input.data == '#') {
+         in.getline(tmp.data(), BUFFER_SIZE, '\n');
+         continue;
+      }
+
+      if (isdigit(input.data)) {
+         size_t dot_count = 0;
+         tmp.clear();
+         tmp[0] = input.data;
+         for (size_t i = 1; i < BUFFER_SIZE; ++i) {
+            input = try_get(in);
+            if (!input.success)
+               break;
+            if (isdigit(input.data) || (input.data == '.' && dot_count == 0))
+               tmp[i] = input.data;
+         }
+      }
+      result = std::stod(tmp);
+   }
+   return result;
+}
 
 InputData read(std::ifstream& in)
 {
-   std::pair<double, double> x_minmax;
-   std::pair<double, double> y_minmax;
-   double area[4];
+   std::pair<double, double> x_minmax { next_number(in), next_number(in) };
+   std::pair<double, double> y_minmax { next_number(in), next_number(in) };
    const size_t areaSize = 4;
    double x, y;
    InputData out;
-   int len = 255;
-   std::string tmp;
-   tmp.resize(len);
-   for (size_t i = 0; i < areaSize; i++) {
-       in.getline(tmp.data(),len);
-       if (tmp.find_first_of('#') != tmp.npos) {
-           i--;
-           continue;
-       }
-       in >> area[i];
+   out.polygon = makePolygonByArea(x_minmax, y_minmax);
+   const size_t points_amount = next_number(in);
+   out.points.resize(points_amount);
+   for (size_t i = 0; i < points_amount; ++i) {
+      out.points[i] = Point(next_number(in), next_number(in));
    }
    return out;
-}
-
-namespace t1_1 {
 }
 
 void task1_1()
@@ -53,15 +99,11 @@ void task1_1()
       Polygon& polygon = data.polygon;
       std::vector<Point>& input = data.points;
       std::cout << "Points in area: \n";
-      for (auto&& i :
-           Polygon(polygon).pointsInsidePolygon(input, Polygon::SIMPLE)) {
+      for (auto&& i : polygon.pointsInsidePolygon(input, Polygon::SIMPLE)) {
          std::cout << "\t" << i << "\n";
       }
    }
    in.close();
-}
-
-namespace t1_2 {
 }
 
 void task1_2()
@@ -73,11 +115,10 @@ void task1_2()
       std::cerr << "File " << in_file << " not exist or not accessible\n";
    } else {
       auto data = read(in);
-      std::vector<Point>& polygon = std::get<0>(data);
-      std::vector<Point>& input = std::get<1>(data);
+      Polygon& polygon = data.polygon;
+      std::vector<Point>& input = data.points;
       std::cout << "Points in area: \n";
-      for (auto&& i :
-           Polygon(polygon).pointsInsidePolygon(input, Polygon::SIMPLE)) {
+      for (auto&& i : polygon.pointsInsidePolygon(input, Polygon::GRID)) {
          std::cout << "\t" << i << "\n";
       }
    }
