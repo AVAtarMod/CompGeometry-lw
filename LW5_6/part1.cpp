d1efa527d3543e63e4908056ea385b7099377cfa
AVAtarMod <Respawner289@gmail.com>
2023-05-24 23:00:39 +0300

    LW5_6: add advanced data parser, update test data Refactor

--- LW5_6/part1.cpp
+++ LW5_6/part1.cpp
@@ -1,19 +1,11 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <tuple>
 #include <vector>
 
 #include "lib_cppgeometry/Polygon.hpp"
 #include "part1.hpp"
-
-Polygon makePolygonByArea(const std::pair<double, double>& x_minmax,
-                          const std::pair<double, double>& y_minmax)
-{
-   const Point a(x_minmax.first, y_minmax.first),
-     b(x_minmax.second, y_minmax.first), c(x_minmax.second, y_minmax.second),
-     d(x_minmax.first, y_minmax.second);
-   return Polygon(std::vector<Point> { a, b, c, d });
-}
 
 struct InputData
 {
@@ -27,7 +19,31 @@ struct IOChar
    bool success = false;
 };
 
-IOChar try_get(std::ifstream& in)
+class CharType
+{
+  public:
+   bool digit = false;
+   bool minus = false;
+   bool plus = false;
+   bool dot = false;
+
+   CharType() {}
+   CharType(char data)
+   {
+      if (data == '.')
+         dot = true;
+      if (data == '-')
+         minus = true;
+      if (data == '+')
+         plus = true;
+      if (isdigit(data))
+         digit = true;
+   }
+   bool isSignChar() const { return minus || plus; }
+   bool isRecognized() const { return digit || minus || plus || dot; }
+};
+
+IOChar try_get(std::istream& in)
 {
    IOChar result;
    in.get(result.data);
@@ -38,7 +54,17 @@ IOChar try_get(std::ifstream& in)
    return result;
 }
 
-double next_number(std::ifstream& in)
+std::string extractBufferContent(const std::string& buffer)
+{
+   std::stringstream stream;
+   for (auto&& i : buffer) {
+      if (i != '\0')
+         stream << i;
+   }
+   return stream.str();
+}
+
+double next_number(std::istream& in)
 {
    const size_t BUFFER_SIZE = 255;
    std::string tmp;
@@ -48,79 +74,127 @@ double next_number(std::ifstream& in)
    while (true) {
       IOChar input = try_get(in);
       if (!input.success)
-         break;
+         throw std::runtime_error("File is empty or unknown error");
       if (input.data == '#') {
          in.getline(tmp.data(), BUFFER_SIZE, '\n');
          continue;
       }
 
-      if (isdigit(input.data)) {
-         size_t dot_count = 0;
-         tmp.clear();
-         tmp[0] = input.data;
-         for (size_t i = 1; i < BUFFER_SIZE; ++i) {
-            input = try_get(in);
-            if (!input.success)
-               break;
-            if (isdigit(input.data) || (input.data == '.' && dot_count == 0))
-               tmp[i] = input.data;
-         }
+      CharType t;
+      size_t dot_count = 0, sign_char_count = 0, digit_count = 0;
+      std::fill(tmp.begin(), tmp.end(), '\0');
+      for (size_t i = 0; i < BUFFER_SIZE; ++i) {
+         if (!input.success)
+            break;
+
+         t = { input.data };
+         if (!t.isRecognized())
+            break;
+
+         if (t.isSignChar())
+            ++sign_char_count;
+         else if (t.digit)
+            ++digit_count;
+         else if (t.dot)
+            ++dot_count;
+
+         if (dot_count > 1 || sign_char_count > 1)
+            break;
+
+         tmp[i] = input.data;
+         input = try_get(in);
+      }
+      if (digit_count == 0) {
+         tmp = extractBufferContent(tmp);
+         std::string msg = "'" + tmp + "',pos=" +
+                           std::to_string(std::abs(in.tellg()) - tmp.size()) +
+                           ". End of file reached before end of number.";
+         throw std::runtime_error(msg);
       }
       result = std::stod(tmp);
+      break;
    }
    return result;
 }
 
-InputData read(std::ifstream& in)
+std::string next_line(std::istream& in, const size_t line_length)
 {
-   std::pair<double, double> x_minmax { next_number(in), next_number(in) };
-   std::pair<double, double> y_minmax { next_number(in), next_number(in) };
-   const size_t areaSize = 4;
-   double x, y;
-   InputData out;
-   out.polygon = makePolygonByArea(x_minmax, y_minmax);
-   const size_t points_amount = next_number(in);
-   out.points.resize(points_amount);
-   for (size_t i = 0; i < points_amount; ++i) {
-      out.points[i] = Point(next_number(in), next_number(in));
+   std::string result;
+   while (true) {
+      result.resize(line_length);
+      std::fill(result.begin(), result.end(), '\0');
+      in.getline(result.data(), line_length, '\n');
+      if (in.fail() || in.gcount() == 0)
+         throw std::runtime_error(
+           "Cannot read line, EOF reached or IO error happened");
+      result = extractBufferContent(result);
+      if (result.find_first_of('#') == result.npos && result != "\n" &&
+          result != "")
+         break;
    }
+
+   return result;
+};
+
+InputData read(std::istream& in)
+{
+   InputData out;
+   try {
+      std::pair<double, double> x_minmax { next_number(in), next_number(in) };
+      std::pair<double, double> y_minmax { next_number(in), next_number(in) };
+
+      out.polygon = Polygon::makeByArea(x_minmax, y_minmax);
+      std::string points_amount_str = next_line(in, 255);
+      if (points_amount_str.find_first_of("EOF") == points_amount_str.npos) {
+         std::stringstream stream(points_amount_str, std::ios::in);
+         const size_t points_amount = next_number(stream);
+         out.points.resize(points_amount);
+         for (size_t i = 0; i < points_amount; ++i) {
+            out.points[i] = Point(next_number(in), next_number(in));
+         }
+      } else {
+         try {
+            while (true) {
+               out.points.push_back({ next_number(in), next_number(in) });
+            }
+         } catch (const std::exception& e) {
+         }
+      }
+   } catch (const std::exception& e) {
+      std::cerr << "Invalid file format. Error: " << e.what() << '\n';
+      exit(EXIT_FAILURE);
+   }
+
    return out;
+}
+
+void task(const char* in_file, Polygon::LocaliztionMethod m)
+{
+   std::ifstream in(in_file);
+
+   if (!in) {
+      std::cerr << "File " << in_file << " not exist or not accessible\n";
+   } else {
+      auto data = read(in);
+      Polygon& polygon = data.polygon;
+      std::vector<Point>& input = data.points;
+      auto ret = polygon.pointsInsidePolygon(input, m);
+      std::cout << ret.size() << " points found in area: \n";
+      for (auto&& i : ret) {
+         std::cout << "\t" << i << "\n";
+      }
+   }
+   in.close();
 }
 
 void task1_1()
 {
    const char* in_file = "task1_1in.txt";
-   std::ifstream in(in_file);
-
-   if (!in) {
-      std::cerr << "File " << in_file << " not exist or not accessible\n";
-   } else {
-      auto data = read(in);
-      Polygon& polygon = data.polygon;
-      std::vector<Point>& input = data.points;
-      std::cout << "Points in area: \n";
-      for (auto&& i : polygon.pointsInsidePolygon(input, Polygon::SIMPLE)) {
-         std::cout << "\t" << i << "\n";
-      }
-   }
-   in.close();
+   task(in_file, Polygon::SIMPLE);
 }
 
 void task1_2()
 {
    const char* in_file = "task1_2in.txt";
-   std::ifstream in(in_file);
-
-   if (!in) {
-      std::cerr << "File " << in_file << " not exist or not accessible\n";
-   } else {
-      auto data = read(in);
-      Polygon& polygon = data.polygon;
-      std::vector<Point>& input = data.points;
-      std::cout << "Points in area: \n";
-      for (auto&& i : polygon.pointsInsidePolygon(input, Polygon::GRID)) {
-         std::cout << "\t" << i << "\n";
-      }
-   }
-   in.close();
+   task(in_file, Polygon::GRID);
 }
